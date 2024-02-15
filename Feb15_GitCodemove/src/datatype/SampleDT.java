7199e879205d41951054d82e7b8b6d1241aa93bf
AnithaSiva-Java <anithasivaraj@gmail.com>
2024-02-15 10:50:10 -0500

    CodePush
    Github Code MOve

---
+++ Feb15_GitCodemove/src/datatype/SampleDT.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class SampleDT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=100;
+int b=200;
+System.out.println(a+b);
+	}
+
+}
---
+++ Feb15_GitCodepush/src/datatypestr/sampleb.java
@@ -0,0 +1,12 @@
+package datatypestr;
+
+public class sampleb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Github movement code";
+		System.out.println(name);
+
+	}
+
+}
