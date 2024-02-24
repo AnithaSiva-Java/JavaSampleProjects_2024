45c3b6be1a7163fb0f3a28af0531afa1d1fde106
AnithaSiva-Java <anithasivaraj@gmail.com>
2024-02-24 00:14:29 -0500

    Tables
    Tables using while loop

---
+++ Feb24_WhileLoop/src/whileloop/tableloop.java
@@ -0,0 +1,21 @@
+package whileloop;
+
+public class tableloop {
+
+	public static void main(String[] args) {
+
+		int a=1;
+		int b=6;
+		
+		while (a <=10)
+		{
+		
+		//System.out.println(a);
+		System.out.println(a +"x" +b +"=" +a*b);
+		
+		a=a+1;
+		}
+		
+	}
+
+}
